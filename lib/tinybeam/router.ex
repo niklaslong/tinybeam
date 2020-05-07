2f9fd4d68cb17834e2ab3fad56f1d0883622984a
nkls <niklas@long.ch>
2020-05-07 21:27:10 +0200

    add macros for routing

---
+++ lib/tinybeam/router.ex
@@ -0,0 +1,19 @@
+defmodule Tinybeam.Router do
+  defmacro __using__(_options) do
+    quote do
+      import Tinybeam.Router
+
+      def match(type, route) do
+        do_match(type, route)
+      end
+    end
+  end
+
+  defmacro get(route, body) do
+    quote do
+      defp do_match(:get, unquote(route)) do
+        unquote(body[:do])
+      end
+    end
+  end
+end
