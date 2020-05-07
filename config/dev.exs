d1f0a88d1794861341ff20bae0248703cab4afc0
nkls <niklas@long.ch>
2020-05-07 14:08:55 +0200

    add config for dev and test

--- config/dev.exs
+++ config/dev.exs
@@ -2,4 +2,5 @@ import Config
 
 config :tinybeam,
   host: "127.0.0.1",
+  host_rs: "127.0.0.1:4000",
   port: 4000
--- config/test.exs
+++ config/test.exs
@@ -2,4 +2,5 @@ import Config
 
 config :tinybeam,
   host: "127.0.0.1",
-  port: 8000
+  host_rs: "127.0.0.1:4001",
+  port: 4001
