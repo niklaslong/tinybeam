45e847dc609798f0852ca2f043d57982039de313
nkls <niklas@long.ch>
2020-05-07 11:11:14 +0200

    add first server test

--- test/tinybeam_test.exs
+++ test/tinybeam_test.exs
@@ -1,8 +1,37 @@
 defmodule TinybeamTest do
   use ExUnit.Case
+  alias Mint
+
+  require IEx
   doctest Tinybeam
 
-  test "greets the world" do
-    assert Tinybeam.hello() == :world
+  test "gets a response from the server" do
+    response = send_request("GET", "/")
+
+    assert response.status == 200
+    assert response.data == "I think it works"
+  end
+
+  defp send_request(method, path, headers \\ [], body \\ "") do
+    host = Application.get_env(:tinybeam, :host)
+    port = Application.get_env(:tinybeam, :port)
+
+    {:ok, conn} = Mint.HTTP.connect(:http, host, port)
+    {:ok, conn, req_ref} = Mint.HTTP.request(conn, method, path, headers, body)
+
+    receive do
+      message -> handle_response(conn, message, req_ref)
+    end
+  end
+
+  defp handle_response(conn, message, req_ref) do
+    {:ok, _conn, responses} = Mint.HTTP.stream(conn, message)
+
+    destructure = fn
+      {type, ^req_ref, data} -> {type, data}
+      {type, ^req_ref} -> {type, req_ref}
+    end
+
+    Map.new(responses, &destructure.(&1))
   end
 end
