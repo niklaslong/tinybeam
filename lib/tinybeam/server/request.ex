2922b3a34e502a38cdd85ef221fe45fbc3729957
nkls <niklas@long.ch>
2020-05-15 20:20:06 +0200

    enforce content and headers keys on request struct

--- lib/tinybeam/server/request.ex
+++ lib/tinybeam/server/request.ex
@@ -1,4 +1,4 @@
 defmodule Tinybeam.Server.Request do
-  @enforce_keys [:req_ref, :method]
-  defstruct [:req_ref, :method, :content]
+  @enforce_keys [:req_ref, :method, :headers, :content]
+  defstruct [:req_ref, :method, :headers, :content]
 end
