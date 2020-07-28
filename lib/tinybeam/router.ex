d9efa20edf07b0d99629acf7ffa8ccd8eaf03f49
nkls <niklas@long.ch>
2020-07-28 18:21:32 +0200

    Capitalise default root message

--- lib/tinybeam/router.ex
+++ lib/tinybeam/router.ex
@@ -30,7 +30,7 @@ defmodule Tinybeam.Router do
 
   def match(:get, "/", request),
     do:
-      Response.new(request.req_ref, 200, "hi, welcome to tinybeam!", [
+      Response.new(request.req_ref, 200, "Hi, welcome to tinybeam!", [
         {"Content-Type", "text/plain"}
       ])
 end
